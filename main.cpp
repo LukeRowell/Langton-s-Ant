ad007d5b4831e355e62a8ec5523edf295ed737b4
Luke Rowell <LukeRowell@users.noreply.github.com>
2018-03-17 16:22:11 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,51 +1,183 @@
 /***************************************************************
 AUTHOR: Luke Rowell
-LAST MODIFIED: 3/12/2018
+LAST MODIFIED: 3/17/2018
 DESCRIPTION: Simulation of Langton's Ant.
 
-CONTROLS:		h	-	activates heat map view
-				n	-	activates black and white view
-				o	-	turns off rendering (increases speed)
+CONTROLS:		h		-	toggles heat map view
+				space	-	toggles simulation slowdown
 ***************************************************************/
 #include <SFML/Graphics.hpp>
+#include <windows.h>
+#include <chrono>
+#include <thread>
 
-int main()
+const int GRID_SIZE = 5000;
+int** heatmapValues = new int*[GRID_SIZE];
+bool showHeatmap = false;
+bool slowdown = true;
+sf::RenderWindow window(sf::VideoMode(1000, 1000), "Langton's Ant");
+sf::Image grid;
+sf::Texture gridTexture;
+sf::Sprite gridSprite;
+sf::Image heatmapGrid;
+sf::Texture heatmapGridTexture;
+sf::Sprite heatmapGridSprite;
+
+DWORD WINAPI moveAnt(LPVOID lpParameter)
 {
-	const int GRID_SIZE = 800;
-	int ITERATIONS_PER_FRAME = 100;
 	int antX = GRID_SIZE / 2;
 	int antY = GRID_SIZE / 2;
 	int antOrientation = 1;
 	float heatmapValue = 0.0f;
-	bool showHeatmap = false;
-	bool renderVisuals = true;
-	sf::RenderWindow window(sf::VideoMode(GRID_SIZE, GRID_SIZE), "Langton's Ant");
-	sf::Image grid;
-	sf::Texture gridTexture;
-	sf::Sprite gridSprite;
-	sf::Image heatmapGrid;
-	sf::Texture heatmapGridTexture;
-	sf::Sprite heatmapGridSprite;
 	sf::Color red(255, 0, 0, 255);
 	sf::Color yellow(255, 255, 0, 255);
 	sf::Color green(0, 255, 0, 255);
 	sf::Color cyan(0, 255, 255, 255);
 	sf::Color blue(0, 0, 255, 255);
 
-	int** heatmapValues = new int* [GRID_SIZE];
+	while (window.isOpen())
+	{
+		//Wrap the ant back around if it runs into the edge of the grid
+		if (antX < 0)
+		{
+			antX = GRID_SIZE - 1;
+		}
 
+		if (antY < 0)
+		{
+			antY = GRID_SIZE - 1;
+		}
+
+		if (antX > GRID_SIZE - 1)
+		{
+			antX = 0;
+		}
+
+		if (antY > GRID_SIZE - 1)
+		{
+			antY = 0;
+		}
+
+		//Normalize the heat map value between 0 and 1
+		heatmapValues[antX][antY]++;
+		heatmapValue = (float)heatmapValues[antX][antY] / 100.0f;
+
+		if (heatmapValue > 1)
+		{
+			//If the cell has exceeded 100 visits, don't update it's color anymore
+		}
+
+		else
+		{
+			//Interpolate the color between the correct ranges
+			if (heatmapValue <= 0.2)	//Blue -> Cyan
+			{
+				heatmapGrid.setPixel(antX, antY, sf::Color((cyan.r - blue.r) * heatmapValue + blue.r, (cyan.g - blue.g) * heatmapValue + blue.g, (cyan.b - blue.b) * heatmapValue + blue.b, 255));
+			}
+
+			else if (heatmapValue > 0.2 && heatmapValue <= 0.4)		//Cyan -> Green
+			{
+				heatmapGrid.setPixel(antX, antY, sf::Color((green.r - cyan.r) * heatmapValue + cyan.r, (green.g - cyan.g) * heatmapValue + cyan.g, (green.b - cyan.b) * heatmapValue + cyan.b, 255));
+			}
+
+			else if (heatmapValue > 0.4 && heatmapValue <= 0.6)		//Green -> Yellow
+			{
+				heatmapGrid.setPixel(antX, antY, sf::Color((yellow.r - green.r) * heatmapValue + green.r, (yellow.g - green.g) * heatmapValue + green.g, (yellow.b - green.b) * heatmapValue + green.b, 255));
+			}
+
+			else if (heatmapValue > 0.6 && heatmapValue <= 0.8)		//Yellow -> Red
+			{
+				heatmapGrid.setPixel(antX, antY, sf::Color((red.r - yellow.r) * heatmapValue + yellow.r, (red.g - yellow.g) * heatmapValue + yellow.g, (red.b - yellow.b) * heatmapValue + yellow.b, 255));
+			}
+
+			else if (heatmapValue > 0.8)		//Red
+			{
+				heatmapGrid.setPixel(antX, antY, red);
+			}
+		}
+
+		if ((grid.getPixel(antX, antY)) == sf::Color::Black)		//If the ant is on a black square
+		{
+			switch (antOrientation)
+			{
+				case 0:		//Left
+					grid.setPixel(antX, antY, sf::Color::White);
+					antX += 1;
+					antOrientation = 3;
+					break;
+
+				case 1:		//Up
+					grid.setPixel(antX, antY, sf::Color::White);
+					antY -= 1;
+					antOrientation--;
+					break;
+
+				case 2:		//Right
+					grid.setPixel(antX, antY, sf::Color::White);
+					antX -= 1;
+					antOrientation--;
+					break;
+
+				case 3:		//Down
+					grid.setPixel(antX, antY, sf::Color::White);
+					antY += 1;
+					antOrientation--;
+					break;
+
+				default:
+					break;
+			};
+		}
+
+		else	//If the ant is on a white square
+		{
+			switch (antOrientation)
+			{
+				case 0:		//Left
+					grid.setPixel(antX, antY, sf::Color::Black);
+					antX -= 1;
+					antOrientation++;
+					break;
+
+				case 1:		//Up
+					grid.setPixel(antX, antY, sf::Color::Black);
+					antY += 1;
+					antOrientation++;
+					break;
+
+				case 2:		//Right
+					grid.setPixel(antX, antY, sf::Color::Black);
+					antX += 1;
+					antOrientation++;
+					break;
+
+				case 3:		//Down
+					grid.setPixel(antX, antY, sf::Color::Black);
+					antY -= 1;
+					antOrientation = 0;
+					break;
+
+				default:
+					break;
+			};
+		}
+
+		if (slowdown)
+		{
+			std::this_thread::sleep_for(std::chrono::microseconds(1));
+		}
+	}
+
+	return 0;
+}
+
+int main()
+{
+	//Create and initialize the 2D array of heat map values
 	for (int i = 0; i < GRID_SIZE; i++)
 	{
 		heatmapValues[i] = new int[GRID_SIZE];
 	}
-
-	grid.create(GRID_SIZE, GRID_SIZE, sf::Color::White);
-	gridTexture.loadFromImage(grid);
-	gridSprite.setTexture(gridTexture);
-
-	heatmapGrid.create(GRID_SIZE, GRID_SIZE, blue);
-	heatmapGridTexture.loadFromImage(heatmapGrid);
-	heatmapGridSprite.setTexture(heatmapGridTexture);
 
 	for (int i = 0; i < GRID_SIZE; i++)
 	{
@@ -55,6 +187,21 @@ int main()
 		}
 	}
 
+	//Initialize the grid size, color, texture, and sprite texture for the regular and heat map grids
+	grid.create(GRID_SIZE, GRID_SIZE, sf::Color::White);
+	gridTexture.loadFromImage(grid);
+	gridSprite.setTexture(gridTexture);
+
+	heatmapGrid.create(GRID_SIZE, GRID_SIZE, sf::Color::Blue);
+	heatmapGridTexture.loadFromImage(heatmapGrid);
+	heatmapGridSprite.setTexture(heatmapGridTexture);
+
+	//Scale the sprites down 1000x1000 so the whole image is shown on the screen
+	gridSprite.scale(sf::Vector2f(0.2, 0.2));
+	heatmapGridSprite.scale(sf::Vector2f(0.2, 0.2));
+
+	HANDLE threadHandle = CreateThread(NULL, 0, moveAnt, NULL, 0, NULL);		//Start up the thread using the function moveAnt()
+
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -63,168 +210,40 @@ int main()
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
-		
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::H))
+
+			//Toggle heat map
+			if (event.type == sf::Event::KeyReleased && event.key.code == sf::Keyboard::H)
 			{
-				showHeatmap = true;
-				renderVisuals = true;
+				showHeatmap = showHeatmap ? false : true;
 			}
 
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::N))
+			//Toggle speed
+			if (event.type == sf::Event::KeyReleased && event.key.code == sf::Keyboard::Space)
 			{
-				showHeatmap = false;
-				renderVisuals = true;
-			}
-
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::O))
-			{
-				renderVisuals = false;
+				slowdown = slowdown ? false : true;
 			}
 		}
-
-		window.clear();
 		
-		for (int i = 0; i < ITERATIONS_PER_FRAME; i++)
+		window.clear();
+
+		//Draw the appropriate grid
+		if (showHeatmap)
 		{
-			if (antX < 0)
-			{
-				antX = GRID_SIZE - 1;
-			}
+			heatmapGridTexture.loadFromImage(heatmapGrid);
+			window.draw(heatmapGridSprite);
+		}
 
-			if (antY < 0)
-			{
-				antY = GRID_SIZE - 1;
-			}
-
-			if (antX > GRID_SIZE - 1)
-			{
-				antX = 0;
-			}
-
-			if (antY > GRID_SIZE - 1)
-			{
-				antY = 0;
-			}
-
-			heatmapValues[antX][antY]++;
-			heatmapValue = (float)heatmapValues[antX][antY] / 100.0f;
-
-			if (heatmapValue > 1)
-			{
-
-			}
-
-			else
-			{
-				if (heatmapValue <= 0.2)	//Blue -> Cyan
-				{
-					heatmapGrid.setPixel(antX, antY, sf::Color((cyan.r - blue.r) * heatmapValue + blue.r, (cyan.g - blue.g) * heatmapValue + blue.g, (cyan.b - blue.b) * heatmapValue + blue.b, 255));
-				}
-
-				else if (heatmapValue > 0.2 && heatmapValue <= 0.4)		//Cyan -> Green
-				{
-					heatmapGrid.setPixel(antX, antY, sf::Color((green.r - cyan.r) * heatmapValue + cyan.r, (green.g - cyan.g) * heatmapValue + cyan.g, (green.b - cyan.b) * heatmapValue + cyan.b, 255));
-				}
-
-				else if (heatmapValue > 0.4 && heatmapValue <= 0.6)		//Green -> Yellow
-				{
-					heatmapGrid.setPixel(antX, antY, sf::Color((yellow.r - green.r) * heatmapValue + green.r, (yellow.g - green.g) * heatmapValue + green.g, (yellow.b - green.b) * heatmapValue + green.b, 255));
-				}
-
-				else if (heatmapValue > 0.6 && heatmapValue <= 0.8)		//Yellow -> Red
-				{
-					heatmapGrid.setPixel(antX, antY, sf::Color((red.r - yellow.r) * heatmapValue + yellow.r, (red.g - yellow.g) * heatmapValue + yellow.g, (red.b - yellow.b) * heatmapValue + yellow.b, 255));
-				}
-
-				else if(heatmapValue > 0.8)		//Red
-				{
-					heatmapGrid.setPixel(antX, antY, red);
-				}
-			}
-
-			if (showHeatmap && renderVisuals)
-			{
-				heatmapGridTexture.loadFromImage(heatmapGrid);
-				window.draw(heatmapGridSprite);
-			}
-
-			if ((grid.getPixel(antX, antY)) == sf::Color::Black)		//If the ant is on a black square
-			{
-				switch (antOrientation)
-				{
-					case 0:		//Left
-						grid.setPixel(antX, antY, sf::Color::White);
-						antX += 1;
-						antOrientation = 3;
-						break;
-
-					case 1:		//Up
-						grid.setPixel(antX, antY, sf::Color::White);
-						antY -= 1;
-						antOrientation--;
-						break;
-
-					case 2:		//Right
-						grid.setPixel(antX, antY, sf::Color::White);
-						antX -= 1;
-						antOrientation--;
-						break;
-
-					case 3:		//Down
-						grid.setPixel(antX, antY, sf::Color::White);
-						antY += 1;
-						antOrientation--;
-						break;
-
-					default:
-						break;
-				};
-			}
-
-			else	//If the ant is on a white square
-			{
-				switch (antOrientation)
-				{
-					case 0:		//Left
-						grid.setPixel(antX, antY, sf::Color::Black);
-						antX -= 1;
-						antOrientation++;
-						break;
-
-					case 1:		//Up
-						grid.setPixel(antX, antY, sf::Color::Black);
-						antY += 1;
-						antOrientation++;
-						break;
-
-					case 2:		//Right
-						grid.setPixel(antX, antY, sf::Color::Black);
-						antX += 1;
-						antOrientation++;
-						break;
-
-					case 3:		//Down
-						grid.setPixel(antX, antY, sf::Color::Black);
-						antY -= 1;
-						antOrientation = 0;
-						break;
-
-					default:
-						break;
-				};
-			}
-
-			if (!showHeatmap && renderVisuals)
-			{
-				gridTexture.loadFromImage(grid);
-				window.draw(gridSprite);
-			}
+		else if (!showHeatmap)
+		{
+			gridTexture.loadFromImage(grid);
+			window.draw(gridSprite);
 		}
 
 		window.display();
 	}
 
-	delete heatmapValues;
+	delete heatmapValues;			//Delete the array of heat map values
+	CloseHandle(threadHandle);		//Kill the thread
 
 	return 0;
 }
